70790428333a6111a888ce8b32d85dc8377add8e
ercole <ercole@ercole-ubuntu.(none)>
2013-11-21 13:14:53 +0100

    log4j new feature added

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ Java Servlet that wraps a generic cli bash command.
 Introduction:
 =============
 Simple, modular, customizable Java Servlet that wraps a cli bash command.
-It can be usefull in context of application architecture reengineering in which the system comunications between modules can be performed with a simple curl command.
+It can be usefull in context of application architecture reengineering in which the internal system comunications between bash commands modules can be performed with a simple curl command.
 For instance where some modules needs to be migrated to other platforms and other modules (more system dipendent) needs to temporary remain on the old system. 
 
 Features:
 =============
-- Allows the copresence of many listening command to different urls;
-- The war file don't needs to be recompiled in case of changing of the wrapped command;
-
+- Allows the copresence of different listening urls which are assigned different command as well;
+- The war file don't needs to be recompiled in case of changing of the wrapped commands or adding of new ones;
+- The log4j logging system is used for the sub process error handling and for others execution problem happened.
 
 Implementation details:
 =============
@@ -28,24 +28,24 @@ Use:
 - Clone the repository;
 - Import into Eclipse in a new dynamic web project;
 - Download from the web the Commons IO - Apache Commons library and importing it on the web project;
-- Customize the web.xml file as preferred; (bash command to execute, http url to listen)
-- Export as a war file;
+- Customize the web.xml file as preferred; (bash command to execute, http url_to_listen)
+- Export as a war file (maybe named view.war) ;
 - Deploying it on a servlet engine (I experienced with tomcat7);
 - Run the servlet engine;
 - Test it with curl:
-    curl --data "input text" localhost:8080/url_to_listen/
+	curl --data-binary @input_file http://localhost:8080/view/url_to_list/
 
 - Enjoy.
 
 
 Problems:
 ==============
-With apache tomcat7 I observed a problem with the "system-wide" installation (which is the one installed by apt-get install ...):
+With apache tomcat7 I observed an issue with the "system-wide" installation (which is the one installed by apt-get install ...):
 The problem was related to the fact that tomcat7 daemon process, belongs to the tomcat7 linux user, has not the rights to execute some customized bash commands due to apache tomcat7 security restrictions.
-The issue was solved by downloading the binary distribution from the ASF official web site and using jsvc apache utility to set it up to a system service daemon. While jsvc allows to rewrite some daemon configuration constraint like the linux user.
+The issue was solved by downloading the binary distribution from the ASF official web site and using jsvc apache utility to set it up to a system service daemon. Which jsvc allows to rewrite some daemon configuration constraint like the execution linux user.
 
 Todo:
 ==============
-- A better implementation of the log system, maybe using log4j;
-- Extend the offered functionality to the http get calls as well;
+- Extend the offered functionality to the http GET calls as well;
+- extends the logger class with viewLogger implementation which in every log call made is show the specific command (the web.xml ones) that generated it;
 - Mavenizing it.
--- src/it/view/view.java
+++ src/it/view/view.java
@@ -3,7 +3,9 @@ package it.view;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.StringWriter;
 import java.lang.ProcessBuilder.Redirect;
+import java.nio.charset.Charset;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -15,7 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
-
+import org.apache.log4j.Logger;
 
 /**
  * Servlet implementation view class
@@ -23,6 +25,8 @@ import org.apache.commons.io.IOUtils;
 @WebServlet("/view")
 public class view extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	private final static Logger log = Logger.getLogger(view.class.getName());
 
 	private String[] command;
 
@@ -34,69 +38,103 @@ public class view extends HttpServlet {
 		// TODO Auto-generated constructor stub
 	}
 
-	public void init(ServletConfig servletConfig) throws ServletException{
-	    this.command = servletConfig.getInitParameter("cmd").split("\\s");
-	  }
-	
+	public void init(ServletConfig servletConfig) throws ServletException {
+		this.command = servletConfig.getInitParameter("cmd").split("\\s");
+	}
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		//TODO:
-		//extend the servlet functionality to the get requests
+
+		// logging phase
+		log.info("Serving Get request");
+
+		// TODO:
+		// extend the servlet functionality to the get requests
 		ServletOutputStream response_data = response.getOutputStream();
 		response_data.write("Lets try with an http post".getBytes());
 		response_data.close();
+
+		log.debug("Served Get request");
 	}
 
 	/**
+	 * @throws IOException
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		// String command = "ls -la";
-		// String log_file="log_file";
-
+		log.info("["+this.command[0]+"]Serving Post request");
+		
 		// configuring the ProcessBuilder with the command str
 		ProcessBuilder builder = new ProcessBuilder(command);
 
 		// configuring the process builder
 		builder.redirectInput(Redirect.PIPE);
 		builder.redirectOutput(Redirect.PIPE);
+
+		// configuring process stderr to be redirected to the log system
+		builder.redirectError(Redirect.PIPE);
+
+		Process p = null;
+		try {
+			log.trace("Starting the sub process");
+					
+			p = builder.start();
+		} catch (IOException e) {
+			
+			// building the command str for passing it to the logger
+			StringBuilder c = new StringBuilder();
+			for (int i = 0; i < command.length; i++) {
+				c.append(command[i]);
+				c.append("[SPACE]");
+			}
+			log.fatal("Failed the command execution:\nCommand:\n"+c.toString(), e);
+			return;
+		}
+
+		log.trace("Process has been started");
 		
-		//TODO:
-		//implement a better error log system (log4j?)
-		builder.redirectError(Redirect.INHERIT);
-		//builder.redirectError(Redirect.PIPE);
-
-		// starting the subprocess
-		Process p = builder.start();
-
 		// get the request and the response streams
 		ServletInputStream request_data = request.getInputStream();
 		ServletOutputStream response_data = response.getOutputStream();
-
+		
+		log.trace("Copying the http request data to the subprocess input");
+		
 		// copying the http request data to the subprocess input
 		OutputStream sub_process_input = p.getOutputStream();
 		IOUtils.copy(request_data, sub_process_input);
 		sub_process_input.close();
 
+		log.trace("Copying the output of the subprocess in the http response");
+		
 		// copying the output of the subprocess in the http response
 		InputStream sub_process_output = p.getInputStream();
 		IOUtils.copy(sub_process_output, response_data);
 		sub_process_output.close();
+	
 		
-		//copying the error stream in the http response (to be changed)
-		/*
+		log.trace("Copying the error stream of the process (if there exists) to the log system");
+		
+		// copying the error stream of the process (if there exists)
+		// to the log system
 		InputStream sub_process_err = p.getErrorStream();
-		IOUtils.copy(sub_process_err, response_data);
+		StringWriter log4j_writer = new StringWriter();
+		IOUtils.copy(sub_process_err, log4j_writer, Charset.forName("UTF-8"));
 		sub_process_err.close();
-		*/
-		
-	}
 
+		String log4jstr = log4j_writer.toString();
+
+		// "if there exists" clause
+		if (log4jstr != null)
+			if (!log4jstr.equals(""))
+				log.fatal(log4jstr);
+
+		log.debug("Served Post request");
+	}
 }
