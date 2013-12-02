90f2799a64264a2c8f54b92293e7ef5f1a2727cb
ercole <ercole@ercole-ubuntu.(none)>
2013-12-02 14:34:05 +0100

    using utf-8 enconding of the errors messages through the log4j system

--- README.md
+++ README.md
@@ -11,23 +11,24 @@ For instance where some modules needs to be migrated to other platforms and othe
 
 Features:
 =============
-- Allows the copresence of different listening urls which are assigned different command as well;
-- The war file don't needs to be recompiled in case of changing of the wrapped commands or adding of new ones;
+- Allows the copresence of different listening urls which are assigned to different commands as well;
+- The war file don't needs to be recompiled in case of changing of the wrapped commands, i.e. adding/removing new ones;
 - The log4j logging system is used for the sub process error handling and for others execution problem happened.
 
 Implementation details:
 =============
-- The bash command is passed once for all through the web.xml configuration file to the servlet engine at deployment time; 
-- It reads the content of the http post and passes it to the standard input of the process instantiated by the command;
-- Returns the stdout of the command in the http post response;
-- It uses Commons IO - Apache Commons for the io comunications with the sub process created.
-
+- The bash command is passed once for all through the web.xml configuration file to the servlet engine at deployment time;
+- In case of changing of the web.xml file the servlet engine (tomcat7 in my case) automatically notices that the file was modified and reload the new servlet context;
+- The servlet reads the content of the http post and passes it to the standard input of the process instantiated by the command;
+- The servlet returns the stdout of the command in the http post response;
+- Uses Commons IO - Apache Commons for the I/O comunications with the sub process created;
+- For logging the stderr of the process the servlet uses the utf-8 encoding. 
 
 Use:
 =============
 - Clone the repository;
 - Import into Eclipse in a new dynamic web project;
-- Download from the web the Commons IO - Apache Commons library and importing it on the web project;
+- Download from the web the Commons IO - Apache Commons library, log4j 2 and importing it on the web project;
 - Customize the web.xml file as preferred; (bash command to execute, http url_to_listen)
 - Export as a war file (maybe named view.war) ;
 - Deploying it on a servlet engine (I experienced with tomcat7);
@@ -43,6 +44,7 @@ Problems:
 With apache tomcat7 I observed an issue with the "system-wide" installation (which is the one installed by apt-get install ...):
 The problem was related to the fact that tomcat7 daemon process, belongs to the tomcat7 linux user, has not the rights to execute some customized bash commands due to apache tomcat7 security restrictions.
 The issue was solved by downloading the binary distribution from the ASF official web site and using jsvc apache utility to set it up to a system service daemon. Which jsvc allows to rewrite some daemon configuration constraint like the execution linux user.
+As a generic suggestion: put the absolute path of the bash command in the web.xml, not the relative one.
 
 Todo:
 ==============
--- src/it/view/view.java
+++ src/it/view/view.java
@@ -3,9 +3,7 @@ package it.view;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.StringWriter;
 import java.lang.ProcessBuilder.Redirect;
-import java.nio.charset.Charset;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -19,9 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 
-/**
- * Servlet implementation view class
- */
+
 @WebServlet("/view")
 public class view extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -35,32 +31,15 @@ public class view extends HttpServlet {
 	 */
 	public view() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public void init(ServletConfig servletConfig) throws ServletException {
 		this.command = servletConfig.getInitParameter("cmd").split("\\s");
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
 
-		// logging phase
-		log.info("Serving Get request");
-
-		// TODO:
-		// extend the servlet functionality to the get requests
-		ServletOutputStream response_data = response.getOutputStream();
-		response_data.write("Lets try with an http post".getBytes());
-		response_data.close();
-
-		log.debug("Served Get request");
-	}
-
+	// TODO: extend the functionality to the get requests
+	
 	/**
 	 * @throws IOException
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
@@ -69,8 +48,8 @@ public class view extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		log.info("["+this.command[0]+"]Serving Post request");
-		
+		log.info("[" + this.command[0] + "]Serving Post request");
+
 		// configuring the ProcessBuilder with the command str
 		ProcessBuilder builder = new ProcessBuilder(command);
 
@@ -84,56 +63,51 @@ public class view extends HttpServlet {
 		Process p = null;
 		try {
 			log.trace("Starting the sub process");
-					
+
 			p = builder.start();
-		} catch (IOException e) {
 			
+			log.trace("Process has been started");
+		} catch (IOException e) {
+
 			// building the command str for passing it to the logger
 			StringBuilder c = new StringBuilder();
 			for (int i = 0; i < command.length; i++) {
 				c.append(command[i]);
 				c.append("[SPACE]");
 			}
-			log.fatal("Failed the command execution:\nCommand:\n"+c.toString(), e);
+			log.fatal(
+					"Failed the command execution:\nCommand:\n" + c.toString(),
+					e);
 			return;
 		}
 
-		log.trace("Process has been started");
-		
-		// get the request and the response streams
-		ServletInputStream request_data = request.getInputStream();
-		ServletOutputStream response_data = response.getOutputStream();
-		
+
 		log.trace("Copying the http request data to the subprocess input");
-		
+
 		// copying the http request data to the subprocess input
 		OutputStream sub_process_input = p.getOutputStream();
+		ServletInputStream request_data = request.getInputStream();
 		IOUtils.copy(request_data, sub_process_input);
 		sub_process_input.close();
+		request_data.close();
 
 		log.trace("Copying the output of the subprocess in the http response");
-		
+
 		// copying the output of the subprocess in the http response
 		InputStream sub_process_output = p.getInputStream();
+		ServletOutputStream response_data = response.getOutputStream();
 		IOUtils.copy(sub_process_output, response_data);
 		sub_process_output.close();
-	
+		response_data.close();
 		
 		log.trace("Copying the error stream of the process (if there exists) to the log system");
-		
-		// copying the error stream of the process (if there exists)
-		// to the log system
-		InputStream sub_process_err = p.getErrorStream();
-		StringWriter log4j_writer = new StringWriter();
-		IOUtils.copy(sub_process_err, log4j_writer, Charset.forName("UTF-8"));
-		sub_process_err.close();
 
-		String log4jstr = log4j_writer.toString();
+		String procErr = IOUtils.toString(p.getErrorStream(), "UTF-8");
+		p.getErrorStream().close();
 
-		// "if there exists" clause
-		if (log4jstr != null)
-			if (!log4jstr.equals(""))
-				log.fatal(log4jstr);
+		if (procErr != null)
+			if (!procErr.equals(""))
+				log.fatal(procErr);
 
 		log.debug("Served Post request");
 	}
